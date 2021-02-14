79b96c1dcd486be5204a2b1d4043eb99ad22a041
pepiyu <58489884+pepiyu@users.noreply.github.com>
2021-02-14 12:34:16 +0100

    flip card

--- pages/index.js
+++ pages/index.js
@@ -19,41 +19,77 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <div id='pug_1' className={styles.card}>
-            <img src="images/chip_2.png" 
-            height="200"/>
-          <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
-            height="200"/>
+          <div id='pug_1' className={styles.flip_card}>
+            <div className={styles.flip_card_inner}>
+              <div className={styles.flip_card_front}>
+                <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
+                  height="200"/>
+              </div>
+              <div className={styles.flip_card_back}>
+                <img src="images/chip_2.png" 
+                height="200"/>
+              </div>
+            </div>
           </div>
-          <div id='pug_2' className={styles.card}>
-            <img src="images/chip_2.png" 
-            height="200"/>
-                      <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
-            height="200"/>
+          <div id='pug_2' className={styles.flip_card}>
+            <div className={styles.flip_card_inner}>
+              <div className={styles.flip_card_front}>
+                <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
+                height="200"/>
+              </div>
+              <div className={styles.flip_card_back}>
+                <img src="images/chip_2.png" 
+                height="200"/>
+              </div>
+            </div>
           </div>
-          <div id='pug_3' className={styles.card}>
-            <img src="images/chip_1.png" 
-            height="200"/>
-            <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
-            height="200"/>
+          <div id='pug_3' className={styles.flip_card}>
+            <div className={styles.flip_card_inner}>
+              <div className={styles.flip_card_front}>
+                <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
+                height="200"/>
+              </div>
+              <div className={styles.flip_card_back}>
+                <img src="images/chip_1.png" 
+                height="200"/>
+              </div>
+            </div>
           </div>
-          <div id='pug_4' className={styles.card}>
-            <img src="images/chip_3.png" 
-            height="200"/>
-            <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
-            height="200"/>
+          <div id='pug_4' className={styles.flip_card}>
+            <div className={styles.flip_card_inner}>
+              <div className={styles.flip_card_front}>
+                <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
+                height="200"/>
+              </div>
+              <div className={styles.flip_card_back}>
+                <img src="images/chip_3.png" 
+                height="200"/>
+              </div>
+            </div>
           </div>
-          <div id='pug_5' className={styles.card}>
-            <img src="images/chip_1.png" 
-            height="200"/>
-            <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
-            height="200"/>
+          <div id='pug_5' className={styles.flip_card}>
+            <div className={styles.flip_card_inner}>
+              <div className={styles.flip_card_front}>
+                <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
+                height="200"/>
+              </div>
+              <div className={styles.flip_card_back}>
+                <img src="images/chip_1.png" 
+                height="200"/>
+              </div>
+            </div>
           </div>
-          <div id='pug_6' className={styles.card}>
-            <img src="images/chip_3.png" 
-            height="200"/>
-            <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
-            height="200"/>
+          <div id='pug_6' className={styles.flip_card}>
+            <div className={styles.flip_card_inner}>
+              <div className={styles.flip_card_front}>
+                <img src="https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667" 
+                height="200"/>
+              </div>
+              <div className={styles.flip_card_back}>
+                <img src="images/chip_3.png" 
+                height="200"/>
+              </div>
+            </div>
           </div>
         </div>
       </main>
