4cadf8ea42402806d58d67a6214d73467850a960
pepiyu <58489884+pepiyu@users.noreply.github.com>
2021-02-14 17:31:50 +0100

    new component nav

---
+++ pages/Components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/Home.module.css'
+
+const Nav = ()=>{
+    return (
+        <div className={styles.nav}>
+                Score: 🐸🐸🐸🐸🐸
+        </div>
+    )
+}
+
+export default Nav
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
+import Nav from './Components/Nav'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Nav></Nav>
       <Head>
         <title>Pug Memory</title>
         <link rel="icon" href="/favicon.ico" />
